5349bd6a1b187d24169257b039446c93e6fd4f01
Alexey Ismagilov <al.a.ismagilov@gmail.com>
2025-04-22 16:39:24 +0200

    Improve script structure

--- convert-into-app.sh
+++ convert-into-app.sh
@@ -1,28 +1,56 @@
 #!/usr/bin/env bash
 
-echo "This script will convert the zathura binary into a macOS App"
+###################
+### Definitions ###
+###################
 
 ZATHURA_EXE_DEFAULT="/opt/homebrew/bin/zathura"
-ZATHURA_EXE_FROM_PATH="$(command -v zathura)"
+ZATHURA_APP="/Applications/Zathura.app"
+ZATHURA_ICON_URL="https://raw.githubusercontent.com/homebrew-zathura/homebrew-zathura/132bb38829938ed8dfdd24f46946aab93f4482e5/icon/zathura-brosasaki.icns"
 
-if [[ -f "${ZATHURA_EXE_DEFAULT}" ]]
+#######################
+### Find executable ###
+#######################
+
+echo "Finding executable..."
+
+ZATHURA_EXE_FROM_PATH=$(command -v zathura)
+
+if [[ -f ${ZATHURA_EXE_DEFAULT} ]]
 then
   echo "zathura executable found at ${ZATHURA_EXE_DEFAULT}"
-  ZATHURA_EXE="${ZATHURA_EXE_DEFAULT}"
-elif [[ -f "${ZATHURA_EXE_FROM_PATH}" ]]
+  ZATHURA_EXE=${ZATHURA_EXE_DEFAULT}
+elif [[ -f ${ZATHURA_EXE_FROM_PATH} ]]
 then
-  echo "zathura executable not found at ${ZATHURA_EXE_DEFAULT}; use ${ZATHURA_EXE_FROM_PATH} from \$PATH instead"
-  ZATHURA_EXE="${ZATHURA_EXE_FROM_PATH}"
+  echo "zathura executable not found at default location (${ZATHURA_EXE_DEFAULT});"
+  echo "use ${ZATHURA_EXE_FROM_PATH} from \$PATH instead"
+  ZATHURA_EXE=${ZATHURA_EXE_FROM_PATH}
 else
   echo "zathura executable not found neither at ${ZATHURA_EXE_DEFAULT}, nor in \$PATH"
   exit 1
 fi
 
-echo "Creating /Applications/Zathura.app"
-mkdir -p /Applications/Zathura.app/Contents/MacOS
-mkdir -p /Applications/Zathura.app/Contents/Resources
-cp "${ZATHURA_EXE}" /Applications/Zathura.app/Contents/MacOS/zathura
-touch /Applications/Zathura.app/Contents/Info.plist
+############################
+### Create app structure ###
+############################
+
+echo "Creating app structure..."
+
+mkdir -p "${ZATHURA_APP}/Contents/MacOS" "${ZATHURA_APP}/Contents/Resources"
+
+#######################
+### Copy executable ###
+#######################
+
+echo "Copying executable..."
+
+cp "${ZATHURA_EXE}" "${ZATHURA_APP}/Contents/MacOS/zathura"
+
+#########################
+### Create Info.plist ###
+#########################
+
+echo "Creating Info.plist..."
 
 read -r -d '' info_plist <<-EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -47,13 +75,21 @@ read -r -d '' info_plist <<-EOF
 </plist>
 EOF
 
-echo "${info_plist}" >/Applications/Zathura.app/Contents/Info.plist
+echo "${info_plist}" >"${ZATHURA_APP}/Contents/Info.plist"
 
-echo "Getting the icon"
-curl -o /Applications/Zathura.app/Contents/Resources/AppIcon.icns https://raw.githubusercontent.com/homebrew-zathura/homebrew-zathura/132bb38829938ed8dfdd24f46946aab93f4482e5/icon/zathura-brosasaki.icns
+#########################
+### Download the icon ###
+#########################
 
-echo "Making it executable"
-chmod +x /Applications/Zathura.app/Contents/MacOS/zathura
+echo "Downloading the icon..."
+
+curl -o "${ZATHURA_APP}/Contents/Resources/AppIcon.icns" "${ZATHURA_ICON_URL}"
+
+###############
+### Caveats ###
+###############
+
+echo "Caveats..."
 
 cat <<EOF
 Now you can run the app by double clicking on it.
